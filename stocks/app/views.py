d370d4a4829741da9655ef27ab33f933ef27f481
TelisK <tkoutsogiannakis@hotmail.com>
2025-12-10 10:33:45 +0200

    comment the views and first step of code on stocks.py

--- stocks/app/views.py
+++ stocks/app/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from .models import Stocks, StocksHistory
+from services.stock import stock_calculation
+import datetime
 
 
 # Create your views here.
 def index(request):
     return render(request, 'app/index.html')
+
+def show_results(request, date):
+    if request.method == 'POST':
+        asset = request.POST.get('asset','')
+        initial = request.POST.get('initial', '')
+        monthly = request.POST.get('monthly', '')
+        startdate = request.POST.get('startdate', '')
+        enddate = request.POST.get('enddate','')
+
+    stock_calculation(asset)
+    data = StocksHistory.objects.get(name=asset)
+    
+    if startdate == '':
+        pass # επεστρεψε να περασει ημερομηνία
+    else:
+        start_date_obj = datetime.strptime(startdate, "%Y-%m-%d").date()
+
